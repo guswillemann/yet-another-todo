f198353f578b9e6d2797a5a52c5824d1aae4855b
Gustavo Willemann <gustavowill@gmail.com>
2025-07-01 01:02:38 -0300

    feat: add todo data context manipulation methods

--- context/TodoDataContext.tsx
+++ context/TodoDataContext.tsx
@@ -1,18 +1,47 @@
 import { Todo } from '@/types';
 import { createContext, FC, ReactNode, useContext, useState } from 'react';
+import uuid from 'react-native-uuid';
 
 type TodoContextValue = {
   todoData: Todo[]
+  addTodo: (newTodoData: Omit<Todo, 'id'>) => void
+  editTodo: (editedTodoData: Todo) => void
+  deleteTodo: (id: string) => void
 };
+
+function newTodoId() {
+  return `local-${uuid.v4()}`
+}
 
 const TodoDataContext = createContext({} as TodoContextValue);
 
 export const TodoDataProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [todoData, setTodoData] = useState<Todo[]>([]);
-  
+
+  const addTodo: TodoContextValue['addTodo'] = (newTodoData) => {
+    setTodoData((prevState) => [...prevState, { ...newTodoData, id: newTodoId() }]);
+  }
+
+  const editTodo: TodoContextValue['editTodo'] = (editedTodoData) => {
+    setTodoData((prevState) => prevState.map((todo) => {
+      if (todo.id === editedTodoData.id) {
+        return editedTodoData;
+      } else {
+        return todo;
+      }
+    }));
+  }
+
+  const deleteTodo: TodoContextValue['deleteTodo'] = (id) => {
+    setTodoData((prevState) => prevState.filter((todo) => todo.id !== id));
+  }
+
   return (
     <TodoDataContext.Provider value={{
-      todoData
+      todoData,
+      addTodo,
+      editTodo,
+      deleteTodo,
     }}>
       {children}
     </TodoDataContext.Provider>
